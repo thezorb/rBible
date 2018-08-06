651bcbc7f42214f82aed83df3c98464750c45d97
Zorbey Canturk <4762427+thezorb@users.noreply.github.com>
2018-08-06 15:59:17 -0400

    Update readingData.r

--- getting started/readingData.r
+++ getting started/readingData.r
@@ -1,2 +1,15 @@
-library(readr)
+#read CSV
+newCSV <- read.csv("filename.csv", header = TRUE)
+
+#read text file
+require(readr)
+readTXT <- read.table("filename.txt",header = TRUE,sep = ";")
+
+#read Excel v1
+library(readxl)
+readExcel <- read_excel("filename.xlsx")
+
+#read Excel v2
+require(gdata)
+df = read.xls ("myfile.xlsx"), sheet = 1, header = TRUE)
 
