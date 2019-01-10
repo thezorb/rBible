5aa3f602a40fd5196ae828b21cc98f1c97508ee2
Zorbey Canturk <4762427+thezorb@users.noreply.github.com>
2019-01-10 17:51:02 -0500

    Update basics.r

--- statistics/basics.r
+++ statistics/basics.r
@@ -67,3 +67,64 @@ ggplot(pois, aes(x=x)) +
   geom_density(aes(group=Lambda, color=Lambda, fill=Lambda), adjust=4, alpha=1/2) + 
   scale_color_discrete() + scale_fill_discrete() +
   ggtitle("Probability Mass function")
+
+x <- sample(x=1:100, size=100, replace=TRUE)
+x
+mean(x)
+#removing nulls while calculating averages
+y<-x
+y[sample(x=1:100, size=20, replace=FALSE)] <- NA
+mean(y)
+mean(y,na.rm=TRUE)
+
+#weighted averages
+grades <- c(95, 72, 87, 66)
+weights <- c(1/2, 1/4, 1/8, 1/8)
+mean(grades)
+weighted.mean(x=grades, w=weights)
+
+#variance: how spread out the data is? smaller variance, closer the values are to the center
+var(x)
+mean(x)
+sum((x-mean(x))^2)/(length(x)-1)
+sqrt(var(x))
+sd(x)
+sd(y)
+sd(y,na.rm=TRUE)
+
+summary(x)
+summary(y)
+
+quantile(x, probs=c(.25,.75))
+quantile(x, probs=c(.1, .25, .5, .75, .92))
+
+require(ggplot2)
+cor(economics$pce, economics$psavert)
+cor(economics[, c(2, 4:6)])
+econCor <- cor(economics[, c(2, 4:6)])
+require(reshape2)
+econMelt <- melt(econCor, varnames=c("x", "y"), value.name = "Correlation")
+econMelt <- econMelth[order(econMelt$Correlation), ]
+econMelt
+require(scales)
+ggplot(econMelt, aes(x=x, y=y)) + 
+  geom_tile(aes(fill=Correlation)) + 
+  scale_fill_gradient2(low=muted("red"), mid="white", high="steelblue", 
+                       guide=guide_colorbar(ticks=FALSE, barheight=10),
+                       limits=c(-1,1)) + 
+  theme_minimal() + labs(x=NULL, NULL)
+
+
+m <- c(9, 9, NA, 3, NA, 5, 8, 1, 10, 4)
+n <- c(2, NA, 1, 6, 6, 4, 1, 1, 6, 7)
+p <- c(8, 4, 3, 9, 10, NA, 3, NA, 9, 9)
+q <- c(10, 10, 7, 8, 4, 2, 8, 5, 5, 2)
+r <- c(1, 9, 7, 6, 5, 6, 2, 7, 9, 10)
+theMat <- cbind(m, n, p, q, r)
+theMat
+cor(theMat)
+cor(theMat, use="everything")
+cor(theMat, use="all.obs")
+cor(theMat, use="complete.obs")
+theMat
+cor(theMat, use="na.or.complete")
